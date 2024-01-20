7e5917861d35aadd1b224ee58608940106fc2552
Nickolai <lindeman072@mail.ru>
2024-01-21 00:00:40 +0400

    add: InputSearh for IndividualPlan

--- src/components/AboutWorker/AboutWorker.tsx
+++ src/components/AboutWorker/AboutWorker.tsx
@@ -1,7 +1,10 @@
 import "./AboutWorker.css";
 import Avatar from "../../assets/Avatar.svg";
+import { useLocation } from "react-router-dom";
+
 
 export default function AboutWorker() {
+  const location = useLocation();
   return (
     <div className="aboutWorker__container-info">
       <img src={Avatar} className="aboutWorker__avatar" alt="аватар сотрудника" />
@@ -9,7 +12,10 @@ export default function AboutWorker() {
         <h2 className="aboutWorker__fio">Иванов Пётр Александрович</h2>
         <p className="aboutWorker__position">Middle аналитик</p>
       </div>
-      <button className="aboutWorker__button">Создать ИПР +</button>
+      <button 
+      className={`aboutWorker__button ${
+        location.pathname === "/list-of-workers/worker" ? "" : "aboutWorker__button_display_none"
+      }`}>Создать ИПР +</button>
     </div>
   );
 }
---
+++ src/components/StatusPlan/StatusPlan.tsx
@@ -0,0 +1,23 @@
+import "./StatusPlan.css";
+// import ArrowRight from "../../../assets/arrowRight.svg";
+// import NoIpr from "../../../assets/iconNoIPR.svg";
+// import AboutWorker from "../../AboutWorker/AboutWorker";
+
+
+export default function StatusPlan() {
+  return (
+    <div className="statusPlan">
+      <div className="worker__container">
+        <h3 className="worker__adress">ИПР</h3>
+        <img
+          // src={ArrowRight}
+          className="worker__arrow"
+          alt="иконка стрелки вправо"
+        />
+        <h3 className="worker__adress">Сотрудники</h3>
+      </div>
+      
+     
+    </div>
+  );
+}
