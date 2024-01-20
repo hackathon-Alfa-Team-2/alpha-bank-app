4ddab886de6dca23a17f81bb4733268c30747698
Nickolai <lindeman072@mail.ru>
2024-01-20 17:09:02 +0400

    feat: just start IndidvidualPlan

--- src/components/Main/IndidvidualPlan/IndidvidualPlan.tsx
+++ src/components/Main/IndidvidualPlan/IndidvidualPlan.tsx
@@ -3,7 +3,25 @@ import "./IndidvidualPlan.css";
 export default function IndidvidualPlan() {
   return (
     <div className="indidvidualPlan">
-      IndidvidualPlan
+      <div className="worker__container">
+        <h3 className="worker__adress">ИПР</h3>
+        <img
+          // src={ArrowRight}
+          className="worker__arrow"
+          alt="иконка стрелки вправо"
+        />
+        <h3 className="worker__adress">Сотрудники</h3>
+      </div>
+      <div className="worker__container-info">
+        {/* <img src={Avatar} className="worker__avatar" alt="аватар сотрудника" /> */}
+        <div className="worker__container-profile">
+          <h2 className="worker__fio">Иванов Пётр Александрович</h2>
+          <p className="worker__position">Middle аналитик</p>
+        </div>
+        <button className="worker__button">Создать ИПР +</button>
+      </div>
+
+      
     </div>
   );
 }
