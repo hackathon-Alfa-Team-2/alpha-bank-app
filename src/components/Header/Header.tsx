96634eab9c9056624e4623c7d64cf2921788e239
denimur <denis.muradov@yandex.ru>
2024-01-22 14:59:00 +0300

    styles:add  sccs module and color variables

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,37 +1,37 @@
-import Navigation from '../Navigation/Navigation';
-import logo from '../../images/Alfa-People-logo.png';
-import profile from '../../images/Profile-photo.png';
-import './Header.css';
+import Navigation from '../Navigation/Navigation'
+import logoUrl from '../../images/Alfa-People-logo.png'
+import profile from '../../images/Profile-photo.png'
+import styles from './Header.module.scss'
 
 export default function Header() {
+  const {
+    header,
+    sideLogo,
+    sideProfile,
+    logo,
+    searchBox,
+    searchIcon,
+    search,
+    icon,
+    iconContent,
+    profileImage,
+  } = styles
   return (
-    <header className='header'>
-      <div className='wrapper logoSide'>
-        <img
-          className='logo'
-          src={logo}
-          alt='логотип'
-        />
+    <header className={header}>
+      <div className={sideLogo}>
+        <img className={logo} src={logoUrl} alt='логотип' />
         <Navigation />
       </div>
-      <div className='wrapper profileSide'>
-        <label className='searchBox'>
-          <i className='searchIcon'></i>
-          <input
-            className='search'
-            type='text'
-            placeholder='Поиск'
-          />
+      <div className={sideProfile}>
+        <label className={searchBox}>
+          <i className={searchIcon}></i>
+          <input className={search} type='text' placeholder='Поиск' />
         </label>
-        <i className='icon'>
-          <span className='iconContent'></span>
+        <i className={icon}>
+          <span className={iconContent}></span>
         </i>
-        <img
-          className='profileImage'
-          src={profile}
-          alt='фото руководителя'
-        />
+        <img className={profileImage} src={profile} alt='фото руководителя' />
       </div>
     </header>
-  );
+  )
 }
