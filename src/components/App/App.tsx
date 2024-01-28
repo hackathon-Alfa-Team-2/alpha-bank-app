13b1fc39ab5fe65dc1d95700034b2b10eecd492f
denimur <denis.muradov@yandex.ru>
2024-01-29 02:58:23 +0300

    remove comment-list from app layout

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,11 @@
 import Layout from '../Layout/Layout'
 import Main from '../Main/Main'
-import CommentList from '../CommentList/CommentList'
 
 import './App.css'
 
 function App() {
   return (
     <Layout>
-      <CommentList />
       <Main />
     </Layout>
   )
