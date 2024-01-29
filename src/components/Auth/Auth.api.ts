40216ebb84cc5c768326278cf52b35af706293d4
teplostanski <teplostanski@yandex.ru>
2024-01-29 12:44:55 +0300

    feat: add dotenv

--- src/components/Auth/Auth.api.ts
+++ src/components/Auth/Auth.api.ts
@@ -1,7 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+const apiUrl = import.meta.env.VITE_API_BASE_URL
+
 export const authApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://127.0.0.1:8000/api/v1/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: apiUrl }),
   endpoints: (builder) => ({
     login: builder.mutation<
       { access: string; refresh: string },
