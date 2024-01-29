2147ae12d044c2dc01b1158d3ee788c3659ba11a
teplostanski <teplostanski@yandex.ru>
2024-01-29 19:02:08 +0300

    fix: login

--- src/components/Auth/Auth.api.ts
+++ src/components/Auth/Auth.api.ts
@@ -1,15 +1,12 @@
+// Auth.api.ts
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { API_BASE_URL, LOGIN } from '../../constants/endpoints'
 
 export const authApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: API_BASE_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://127.0.0.1:8000/api/v1/' }),
   endpoints: (builder) => ({
-    login: builder.mutation<
-      { access: string; refresh: string },
-      { email: string; password: string }
-    >({
+    login: builder.mutation<string, { email: string; password: string }>({
       query: (body) => ({
-        url: LOGIN,
+        url: 'auth/token/login/',
         method: 'POST',
         body,
       }),
--- src/components/Auth/Auth.context.tsx
+++ src/components/Auth/Auth.context.tsx
@@ -35,30 +35,58 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({
       setIsLoading(true)
       const response = await loginMutation({ email, password })
 
-      if ('auth_token' in response) {
-        const authToken: string | undefined = response.auth_token as string
-
-        if (typeof authToken === 'string') {
-          // Устанавливаем флаг аутентификации
-          setAuthenticated(true)
-
-          // Сохраняем токен в localStorage
-          localStorage.setItem('access_token', authToken)
-
-          console.log('Login successful', authToken)
-        } else {
-          setError('Token is not a string.')
-          console.error('Login failed. Token is not a string.')
+      // Проверяем, является ли ответ строкой
+      if (typeof response === 'string') {
+        // Пытаемся распарсить ответ как JSON
+        let data
+        try {
+          data = JSON.parse(response)
+        } catch (parseError) {
+          setError('Failed to parse response data.')
+          console.error(
+            'Login failed. Failed to parse response data.',
+            response,
+          )
+          return
         }
+
+        handleData(data)
+      } else if ('data' in response) {
+        // Если ответ не строка, то продолжаем обработку, как раньше
+        const data = response.data
+
+        handleData(data)
+      } else if ('error' in response) {
+        setError('Error in response.')
+        console.error('Login failed. Error in response.', response.error)
       } else {
         setError('Unexpected response format.')
-        console.error('Login failed. Unexpected response format.')
+        console.error('Login failed. Unexpected response format.', response)
       }
     } catch (error: any) {
       setError(error.message || 'An error occurred.')
       console.error('Login failed', error)
     } finally {
       setIsLoading(false)
+    }
+  }
+
+  const handleData = (data: any) => {
+    // Проверяем, есть ли свойство "auth_token" в data
+    if ('auth_token' in data) {
+      const authToken: unknown = data.auth_token
+
+      if (typeof authToken === 'string') {
+        setAuthenticated(true)
+        localStorage.setItem('access_token', authToken)
+        console.log('Login successful', authToken)
+      } else {
+        setError('Token is not a string.')
+        console.error('Login failed. Token is not a string.', data)
+      }
+    } else {
+      setError('Unexpected response format.')
+      console.error('Login failed. Unexpected response format.', data)
     }
   }
 
