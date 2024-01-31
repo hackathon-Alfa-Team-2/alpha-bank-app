a5f26b7c2c06a791188a4fe83082a5abd4821a38
teplostanski <teplostanski@yandex.ru>
2024-01-31 21:54:00 +0300

    feat: add routes with roles

--- src/components/Auth/Auth.api.ts
+++ src/components/Auth/Auth.api.ts
@@ -1,8 +1,28 @@
-// Auth.api.ts
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+export interface UserData {
+  id: number
+  first_name: string
+  second_name: string
+  last_name: string
+  position: string
+  grade: string
+  role: string
+  photo: string | null
+}
+
 export const authApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://127.0.0.1:8000/api/v1/' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://127.0.0.1:8000/api/v1/',
+    prepareHeaders: (headers) => {
+      const token = localStorage.getItem('access_token')
+      if (token) {
+        headers.set('Authorization', `Token ${token}`)
+        console.log('Authorization header set:', `Token ${token}`)
+      }
+      return headers
+    },
+  }),
   endpoints: (builder) => ({
     login: builder.mutation<string, { email: string; password: string }>({
       query: (body) => ({
@@ -11,7 +31,10 @@ export const authApi = createApi({
         body,
       }),
     }),
+    getUserData: builder.query<UserData, void>({
+      query: () => 'users/me/',
+    }),
   }),
 })
 
-export const { useLoginMutation } = authApi
+export const { useLoginMutation, useGetUserDataQuery } = authApi
--- src/components/Main/MainRoutes.tsx
+++ src/components/Main/MainRoutes.tsx
@@ -1,6 +1,6 @@
 // MainRoutes.tsx
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Worker from './Worker/Worker'
 import ListWorkers from './ListWorkers/ListWorkers'
 import Main from './Main'
@@ -9,52 +9,70 @@ import Profile from './Profile/Profile'
 import CurrentPlan from './CurrentPlan/CurrentPlan'
 import { Auth } from '../Auth/Auth'
 import ProtectedRoute from '../../utils/ProtectedRoute'
+import { useGetUserDataQuery } from '../Auth/Auth.api'
 
 const MainRoutes: React.FC = () => {
+  const { data: userData, isLoading } = useGetUserDataQuery()
+
+  if (!userData || isLoading) {
+    return <div>Загрузка...</div>
+  }
+
+  console.log(userData?.role)
+
   return (
     <Main>
       <Routes>
         <Route path='/auth' element={<Auth />} />
-        <Route
-          path='/employees'
-          element={
-            <ProtectedRoute>
-              <ListWorkers />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path='/employees/user'
-          element={
-            <ProtectedRoute>
-              <Worker />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path='/employees/user/lms'
-          element={
-            <ProtectedRoute>
-              <IndidvidualPlan />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path='/profile'
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path='/profile/lms'
-          element={
-            <ProtectedRoute>
-              <CurrentPlan />
-            </ProtectedRoute>
-          }
-        />
+        {userData?.role === 'supervisor' && (
+          <>
+            <Route
+              path='/employees'
+              element={
+                <ProtectedRoute>
+                  <ListWorkers />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/employees/user'
+              element={
+                <ProtectedRoute>
+                  <Worker />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/employees/user/lms'
+              element={
+                <ProtectedRoute>
+                  <IndidvidualPlan />
+                </ProtectedRoute>
+              }
+            />
+            <Route path='/*' element={<Navigate to='/employees' replace />} />
+          </>
+        )}
+        {userData?.role === 'employee' && (
+          <>
+            <Route
+              path='/profile'
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/profile/lms'
+              element={
+                <ProtectedRoute>
+                  <CurrentPlan />
+                </ProtectedRoute>
+              }
+            />
+          </>
+        )}
       </Routes>
     </Main>
   )
